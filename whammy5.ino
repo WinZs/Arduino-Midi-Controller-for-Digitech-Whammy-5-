6facc6e49193640f9298df0a38a47a37d0ef530a
Vincent Marguet <32431841+WinZs@users.noreply.github.com>
2017-11-12 11:41:04 +0100

    arduino sketch

--- whammy5.ino
+++ whammy5.ino
@@ -4,10 +4,10 @@
 #include <midi_Namespace.h>
 #include <midi_Settings.h>
 
-byte Bouton1 = 4; 
-byte Bouton2 = 5;
-byte Bouton3 = 6;
-byte Bouton4 = 7;
+byte Bouton1 = 4; //input digital pin on arduino
+byte Bouton2 = 5; //input digital pin on arduino
+byte Bouton3 = 6; //input digital pin on arduino
+byte Bouton4 = 7; //input digital pin on arduino
 byte i = 0 ; 
 MIDI_CREATE_INSTANCE(HardwareSerial,Serial, midiOut); // create a MIDI object called midiOut
 
@@ -20,12 +20,12 @@ void setup() {
 }
 
 void loop() {
-if (digitalRead(Bouton1) == HIGH )   midiOut.sendProgramChange(11, 1);
+if (digitalRead(Bouton1) == HIGH )   midiOut.sendProgramChange(11, 1); // 11 swallow
 
-if (digitalRead(Bouton2) == HIGH )   midiOut.sendProgramChange(20, 1);
+if (digitalRead(Bouton2) == HIGH )   midiOut.sendProgramChange(20, 1); // 20 harmony oct
 
-if (digitalRead(Bouton3) == HIGH )   midiOut.sendProgramChange(0, 1);
+if (digitalRead(Bouton3) == HIGH )   midiOut.sendProgramChange(0, 1); // 0 whammy 2 oct up
 
-if (digitalRead(Bouton4) == HIGH )   midiOut.sendProgramChange(1, 1);
+if (digitalRead(Bouton4) == HIGH )   midiOut.sendProgramChange(1, 1); // 1 whammy oct up
 
 }
